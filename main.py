2aa870d3d6654cfdd8426f7fd33f6cef5906d818
vladi <vladimirbabaev74@gmail.com>
2023-01-11 23:08:45 +0700

    С проверкой на исключения

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ i = 0
 for name in read_data['client']:
     print(i + 1, "элемент имеет значение", read_data['client'][i]["name"])
     i += 1
-b = input('Выберете интересующий элемент словаря: ')
-a = read_data['client'][0:][int(b)-1]
 
-print(a)
-
-#print(read_data.get('client')[])
-
-#print(read_data.get('name', 'Kamal Hossain'))
+while True:
+    try:
+        value = int(input('Введите интересующий элемент словаря '))
+        a = read_data['client'][0:][value - 1]
+        print(a)
+        break
+    except ValueError:
+        print("Введите цифру!")
+    except IndexError:
+        print("У Вас,", d, "значения! А вы вводите", value,". Попробуйте еще раз")
